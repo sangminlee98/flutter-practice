c99dc17855f1870df0a4a19820418fbe7fd44cd5
sangminlee98 <tkdals0975@naver.com>
2023-07-11 17:26:41 +0900

    feat : 프로젝트 초기 세팅

---
+++ lib/screens/onboding/onboding_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [RiveAnimation.asset("assets/RiveAssets/shapes.riv")],
+      ),
+    );
+  }
+}
